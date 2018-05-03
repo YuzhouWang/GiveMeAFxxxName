6626ac45e2dbcf073535e243bd4b7df1ec45839c
stella7 <stellamini88@gmail.com>
2018-05-02 21:30:57 -0400

    build mongodb

---
+++ server/models/questions.model.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const QuestionSchema = mongoose.Schema({
+    //id: String,
+    title: String,
+    txt: String
+},{
+    timestamp: true
+})
+
+module.exports = mongoose.model('Question',QuestionSchema)
